1c3dc439f02b7582a281324422d27f55b78243e5
Tommaso <tommaso.buoso@gmail.com>
2020-06-18 14:03:06 +0200

    NEW: base structure defined

---
+++ main_app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+import urllib.request
+from urllib.parse import urlparse
+
+# config
+app = Flask(__name__)
+
+# Route for handling the index page
+@app.route('/')
+@app.route('/home')
+def home():
+	image_file = url_for('static', filename='images/home.jpg')
+	return render_template('index.html', title='Home', image_file=image_file)
+
+@app.route('/', methods=['POST'])
+@app.route('/home', methods=['POST'])
+def go_to():
+	url = request.form['text']
+	parsed_url = urlparse(url)
+	print(parsed_url.path)
+	#return redirect(url)
+	return urllib.request.urlopen(url).read()
+
+@app.route('/post1')
+def get_post1():
+	return render_template('post1.html')
+
+@app.route('/post2')
+def get_post2():
+	return render_template('post2.html')
+
+# start server
+if __name__ == '__main__':
+	app.run(debug=True)
